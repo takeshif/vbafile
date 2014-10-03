c4830162e4bc3b11763366aa51a549f4630e757e
takeshif <jinko.tatu@gmail.com>
2014-10-03 10:37:31 +0900

    新しくファイルを追加
    SQL　新しくファイルを追加

---
+++ 初回導入紹介内訳作成.sql
@@ -0,0 +1,8 @@
+select itm.group_code, itm.supplier_no, sup.supplier_name, pln.plan_no, pln.plan_title, pln.limit_date,count(chd.item_cd)
+  from m_plan pln, m_plan_child chd, m_item itm, m_supplier sup 
+  where pln.plan_no = chd.plan_no and
+        chd.item_cd = itm.item_cd and
+        itm.supplier_no = sup.supplier_no and
+        pln.plan_no >= 21148 and
+        pln.plan_no <= 21149
+  group by itm.group_code, itm.supplier_no, sup.supplier_name, pln.plan_no, pln.plan_title, pln.limit_date
